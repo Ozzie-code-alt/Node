96aca6e37bb43175a76c8ce5e4df389528a2187b
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-07-31 21:34:41 +0800

    WTH

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+const port = 8383
+
+
+app.get('/', (req, res)=>{
+    res.status(200).send('<h1>Hello World</h1>')
+} )
+
+app.listen(port, ()=> console.log(`Server has started on Port ${port}`))
